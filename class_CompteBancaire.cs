1110cae9dbb6e7655d5c30183b519fe008bb6fd9
Subnaut <101887865+Subnaut@users.noreply.github.com>
2024-02-21 16:36:35 +0100

    push

---
+++ class_CompteBancaire.cs
@@ -0,0 +1,14 @@
+public class CompteBancaire
+{
+				public string	titulaire;
+				public double	solde;
+				public string	devise;
+				//	Constructeur
+				public CompteBancaire(string	leTitulaire,	double	soldeInitial,	string	laDevise)
+				{
+								titulaire	=	leTitulaire;
+								solde	=	soldeInitial;
+								devise	=	laDevise;
+				}
+				//	...
+}
